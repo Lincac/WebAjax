955b503667d43cbe36b1e32caecbf5c096064fe2
Lincaca <2860807719@qq.com>
2022-03-23 18:48:35 +0800

    asdfgh

--- src/com/servlet/Reporting.java
+++ src/com/servlet/Reporting.java
@@ -3,6 +3,7 @@ package com.servlet;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import javafx.util.converter.ShortStringConverter;
 
 import java.io.IOException;
 
@@ -15,6 +16,10 @@ public class Reporting extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String identify = request.getParameter("identify");
+        String name = request.getParameter("name");
+        String college = request.getParameter("college");
+        System.out.println(identify+name+college);
 
     }
 }

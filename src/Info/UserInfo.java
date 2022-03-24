eddafc5eba354ca3648c7a5f8b5659096e614c0c
Lincaca <2860807719@qq.com>
2022-03-24 14:40:55 +0800

    new

--- src/Info/UserInfo.java
+++ src/Info/UserInfo.java
@@ -1,9 +1,22 @@
 package Info;
 
 public class UserInfo {
+    private String identify;
     private String name;
-    private String pass;
-    private int age;
+    private String sex;
+    private String province;
+    private String city;
+    private String susp;
+    private String diag;
+    private String date;
+
+    public String getIdentify() {
+        return identify;
+    }
+
+    public void setIdentify(String identify) {
+        this.identify = identify;
+    }
 
     public String getName() {
         return name;
@@ -13,19 +26,51 @@ public class UserInfo {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
+    public String getSex() {
+        return sex;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 
-    public String getPass() {
-        return pass;
+    public String getProvince() {
+        return province;
     }
 
-    public void setPass(String pass) {
-        this.pass = pass;
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSusp() {
+        return susp;
+    }
+
+    public void setSusp(String susp) {
+        this.susp = susp;
+    }
+
+    public String getDiag() {
+        return diag;
+    }
+
+    public void setDiag(String diag) {
+        this.diag = diag;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
